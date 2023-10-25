2f8f29fb20a984d2480ecbcfadf2d46b5077b997
Jesus Calderin <jesus.c@servicepad.com>
2023-10-25 09:30:55 -0400

    add profile picture to contact chat

--- src/components/atoms/ContactChat.vue
+++ src/components/atoms/ContactChat.vue
@@ -1,4 +1,5 @@
 <script setup>
+import UserPicture from "@/components/atoms/UserPicture.vue";
 defineProps({
   name: String,
   resume: String,
@@ -7,26 +8,36 @@ defineProps({
 
 <template>
   <div class="contact">
-    <h6 class="name">{{ name }}</h6>
-    <small class="resume">{{ resume }}</small>
+    <UserPicture />
+    <div class="contact-info">
+      <h6 class="name">{{ name }}</h6>
+      <small class="resume">{{ resume }}</small>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .contact {
   padding: 0.5rem 1.5rem;
+  gap: 0.5rem;
+  display: flex;
+  align-items: center;
   &:hover {
     background-color: #ffffff21;
   }
-  .name {
-    font-size: 1rem;
-    line-height: 1.1;
-    color: #ffffff;
-  }
-  .resume {
-    font-size: 0.8rem;
-    line-height: 1.1;
-    color: #cacaca;
+  .contact-info {
+    .name {
+      font-size: 1rem;
+      line-height: 1.1;
+      color: #ffffff;
+    }
+    .resume {
+      font-size: 0.8rem;
+      line-height: 1.1;
+      color: #cacaca;
+      padding: 0;
+      margin: 0;
+    }
   }
 }
 </style>
